7ccc67268dcc274965a0e52b03ec095159d4de9d
mocha <2795757650@qq.com>
2024-12-09 01:35:14 +0800

    补充了用户统计查询，订单统计查询，销量排名TOP10查询

--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -1,6 +1,9 @@
 package com.sky.service;
 
+import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 
 import java.time.LocalDate;
 
@@ -13,4 +16,33 @@ public interface ReportService {
      * @return
      */
     TurnoverReportVO getTurnover(LocalDate begin, LocalDate end);
+
+
+    /**
+     * 根据时间区间统计用户数量
+     *
+     * @param begin
+     * @param end
+     * @return
+     */
+    UserReportVO getUserStatistics(LocalDate begin, LocalDate end);
+
+
+    /**
+     * 根据时间区间统计数量
+     *
+     * @param begin
+     * @param end
+     * @return
+     */
+    OrderReportVO getOrderStatistics(LocalDate begin, LocalDate end);
+
+    /**
+     * 查询指定时间区间的销量排名top10
+     *
+     * @param begin
+     * @param end
+     * @return
+     */
+    SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end);
 }
